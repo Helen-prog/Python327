c4ec0a11c347d9ef7f06ad40e118bff64ac50002
Helen <elena.kozovyakina@gmail.com>
2024-01-25 22:01:01 +0300

    blog

--- fifth/programming_blog/programming_blog/settings.py
+++ fifth/programming_blog/programming_blog/settings.py
@@ -110,13 +110,14 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
